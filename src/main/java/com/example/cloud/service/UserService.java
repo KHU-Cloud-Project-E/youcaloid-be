81e68825798de02bc1c00c53b1b38d90909ab8f6
Baejjjjjjjj <rew1212@khu.ac.kr>
2023-11-25 05:02:14 +0900

    :sparkles: feature: 12-feature add service

---
+++ src/main/java/com/example/cloud/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.cloud.service;
+
+public interface UserService {
+
+
+
+    String updateUsernickname(String nickname, Long userId);
+
+    Integer updateUserStatus(int i, Long userId);
+}
