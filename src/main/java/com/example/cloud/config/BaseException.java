c337fd23e56ea415861bce8a4fa17f37e6a8a10c
Baejjjjjjjj <rew1212@khu.ac.kr>
2023-11-25 04:28:20 +0900

    :sparkles: feature: add BaseException

---
+++ src/main/java/com/example/cloud/config/BaseException.java
@@ -0,0 +1,13 @@
+package com.example.cloud.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends Exception {
+    private Code code;
+}
+
